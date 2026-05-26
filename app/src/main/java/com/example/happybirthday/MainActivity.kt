c4979a4a327e188c01f14d25e8dd26c8996848bd
Samantha1919 <samantha.schif@gmail.com>
2026-05-26 09:36:09 +0200

    feat:add opacity example in text

--- app/src/main/java/com/example/happybirthday/MainActivity.kt
+++ app/src/main/java/com/example/happybirthday/MainActivity.kt
@@ -17,6 +17,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
@@ -92,6 +93,7 @@ fun GreetingImage(message: String, from: String, modifier: Modifier = Modifier) 
             modifier = modifier
                 .fillMaxSize()
                 .padding(8.dp)
+                .alpha(0.5F)
         )
 
 
